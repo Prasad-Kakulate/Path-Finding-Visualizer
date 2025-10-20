781d4425a3e31edcf5cde517401aadfdff3f59e2
Devvrat Kokane <devvratkokane@gmail.com>
2025-10-20 14:03:24 +0530

    Add project files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import PathfindingVisualizer from '../src/pathfindingVisualizer/pathfindingVisualizer';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
